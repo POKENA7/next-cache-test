be9a587117b3d13fad13e8574dc537fb1328898d
POKENA7 <pokena191@gmail.com>
2023-09-09 18:57:41 +0900

    検証用コード追加

--- my-app/src/app/getBaseUrl.ts
+++ my-app/src/app/getBaseUrl.ts
@@ -1,4 +1,4 @@
 export const getBaseUrl = () =>
   process.env.VERCEL_URL
-    ? process.env.VERCEL_URL
+    ? "https://next-cache-test-chi.vercel.app"
     : `http://localhost:${process.env.PORT ?? 3000}`;
