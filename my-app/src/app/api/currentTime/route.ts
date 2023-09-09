5c71198081a91305de63fc0a7ccb5d3868df0062
POKENA7 <pokena191@gmail.com>
2023-09-09 20:07:06 +0900

    検証用コード追加

--- my-app/src/app/api/currentTime/route.ts
+++ my-app/src/app/api/currentTime/route.ts
@@ -3,14 +3,16 @@ import { NextRequest, NextResponse } from "next/server";
 let requestedCount = 0;
 
 const getCurrentTime = async () => {
-  let d = new Date();
+  // let d = new Date();
 
-  let hour = d.getHours().toString().padStart(2, "0");
-  let minute = d.getMinutes().toString().padStart(2, "0");
-  let seconds = d.getSeconds().toString().padStart(2, "0");
-  const currentTime = hour + ":" + minute + ":" + seconds;
-  console.log(currentTime);
-
+  // let hour = d.getHours().toString().padStart(2, "0");
+  // let minute = d.getMinutes().toString().padStart(2, "0");
+  // let seconds = d.getSeconds().toString().padStart(2, "0");
+  // const currentTime = hour + ":" + minute + ":" + seconds;
+  // console.log(currentTime);
+  const currentTime = await fetch(
+    "https://timeapi.io/api/Time/current/zone?timeZone=Europe/Amsterdam"
+  );
   requestedCount++;
 
   return { currentTime, requestedCount };
